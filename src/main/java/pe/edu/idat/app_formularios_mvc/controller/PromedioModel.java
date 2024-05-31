0fa2bc9d91a9a42de746868542e08b3201203095
7ezuz <gamarrajesus512@gmail.com>
2024-05-30 22:27:50 -0500

    Vista vs3

---
+++ src/main/java/pe/edu/idat/app_formularios_mvc/controller/PromedioModel.java
@@ -0,0 +1,12 @@
+package pe.edu.idat.app_formularios_mvc.controller;
+
+import lombok.Data;
+
+@Data
+public class PromedioModel {
+    private Integer ec1;
+    private Integer ec2;
+    private Integer ec3;
+    private Integer ef;
+    }
+}
